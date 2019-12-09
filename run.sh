12a49070eddd7eb523fba3ad754c701ffee7fd87
PBearson <peabryan95@gmail.com>
2019-12-09 17:51:56 +0000

    Now we do not stop the container until the updated image has been re-built. Minimizes downtime.

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ TAG="1.0"
 CONTAINER_NAME="bpearson-website-instance"
 PORT=8080
 
+docker build -t $IMAGE_NAME:$TAG .
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
-docker build -t $IMAGE_NAME:$TAG .
 docker run -d -p $PORT:8080 --name $CONTAINER_NAME --restart always $IMAGE_NAME:$TAG
 docker rmi $(docker images -f "dangling=true" -q)
